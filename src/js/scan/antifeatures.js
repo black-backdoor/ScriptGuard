91f5f9eed3e28822bd53beb98e2b7410042153b2
black-backdoor <145743369+black-backdoor@users.noreply.github.com>
2024-01-29 08:14:14 +0100

    + bugfix for displayMetaData
    undefined variables

--- src/js/scan/antifeatures.js
+++ src/js/scan/antifeatures.js
@@ -1,11 +1,11 @@
 function displayAntifeatures(antifeatures) {
     if (antifeatures == undefined || antifeatures == null) {
-        console.debug("EXIT [Antifeatures] Antifeatures is undefined or null");
+        console.log("EXIT [Antifeatures] Antifeatures is undefined or null");
         return;
     }
     
     if (antifeatures.length == 0) {
-        console.debug("EXIT [Antifeatures] No antifeatures found");
+        console.log("EXIT [Antifeatures] No antifeatures found");
         return;
     }
 
--- src/js/scan/scanner.js
+++ src/js/scan/scanner.js
@@ -29,11 +29,11 @@ function main(code) {
 
 function displayMetaData(metaObject) {
     // display the metadata (certian parts)
-    const scriptName = metaObject?.name[0];
-    const scriptVersion = metaObject?.version[0];
-    const scriptDescription = metaObject?.description[0];
-    const scriptAuthor = metaObject?.author[0];
-    const updateURL = metaObject?.updateURL[0] || metaObject?.downloadURL[0];
+    const scriptName = metaObject?.name?.[0] || undefined;
+    const scriptVersion = metaObject?.version?.[0] || undefined;
+    const scriptDescription = metaObject?.description?.[0] || undefined;
+    const scriptAuthor = metaObject?.author?.[0] || undefined;
+    const updateURL = metaObject?.updateURL?.[0] || metaObject?.downloadURL?.[0] || undefined;    
     
     document.getElementById("script-name").textContent = scriptName;
     document.getElementById("script-version").textContent = scriptVersion;
